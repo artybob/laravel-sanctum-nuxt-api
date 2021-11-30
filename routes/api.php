c044297e3bbe401ad3dbb45947861b369513449a
Artem Grafsky <nova1hazard@gmail.com>
2021-11-30 14:02:05 +0700

    passport

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/user', function (Request $request) {
         return new UserResource($request->user());
     });
